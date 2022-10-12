c09f0f626813c8b96ef810c6e845b59845f5e292
stunde168 <stunde168@gmail.com>
2022-10-12 08:19:27 +0300

    Установка обработчика  room в нем же

--- equipmentbot.py
+++ equipmentbot.py
@@ -34,38 +34,38 @@ def sendbuttons(call):
     else:
         msg = bot.send_message(call.message.chat.id, "Ошибка бота 1")
 
-#@bot.message_handler(content_types=["text"])
 def process_room_step(m):
     if not is_int(m.text) :
-        return bot.send_message(m.chat.id, "Введите номер компрессорной")
-    else :
-        room = int(m.text)
-        eqs = get_equipment.get_room(room)
-        strmessage=''
-        for eq in eqs:
-            id = eq["id"]
-            equipment = eq["equipment"]
-            strmessage = strmessage + f"{id} : {equipment}\n"
-        bot.send_message(m.chat.id, strmessage)
+        msg = bot.send_message(m.chat.id, "Введите номер компрессорной")
+        bot.register_next_step_handler(msg, process_room_step)
+        return
+   
+    room = int(m.text)
+    eqs = get_equipment.get_room(room)
+    strmessage=''
+    for eq in eqs:
+        id = eq["id"]
+        equipment = eq["equipment"]
+        strmessage = strmessage + f"{id} : {equipment}\n"
+    bot.send_message(m.chat.id, strmessage)
 
-#@bot.message_handler(content_types=["text"])
 def process_equipment_step(m): 
     if not is_int(m.text) :
         return bot.send_message(m.chat.id, "Введите номер оборудования")
-    else:
-        id = int(m.text)
-        eq = get_equipment.get_equipment(id)
-        id = eq["id"]
-        equipment = eq["equipment"]
-        parametr = eq["parametr"]
-        note = eq["note"]
-        strmessage = f"НАЗВАНИЕ ОБОРУДОВАНИЯ: {equipment} \nПАРАМЕТРЫ: {parametr} \nОПИСАНИЕ: {note}"
-        bot.send_message(m.chat.id, strmessage)
+
+    id = int(m.text)
+    eq = get_equipment.get_equipment(id)
+    id = eq["id"]
+    equipment = eq["equipment"]
+    parametr = eq["parametr"]
+    note = eq["note"]
+    strmessage = f"НАЗВАНИЕ ОБОРУДОВАНИЯ: {equipment} \nПАРАМЕТРЫ: {parametr} \nОПИСАНИЕ: {note}"
+    bot.send_message(m.chat.id, strmessage)
 
 @bot.message_handler(content_types=["text"])
 def handle_text_step0(m):
-    print(f'message.chat.id={m.chat.id} You write {m.text}')
-    if m.text == "t" or m.text == "T" or m.text == "test":
+    print(f'message.chat.id={m.chat.id} message_handler. You write {m.text}')
+    if m.text == "t" or m.text == "T" or m.text == "test" or m.text == 'т' or m.text == 'Т':
         return send_buttons_step0(m)
     process_equipment_step(m)
 
