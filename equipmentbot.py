b5979e019319e10b5a005491f9df53ef8cea16b6
Алексей <rumactep@gmail.com>
2022-10-21 14:35:53 +0300

    + is_blocked_chat типа ScadaAlarms убрано удаление кнопок клавиатуры в /cancel + chat_states[m.chat.id] = (id, listfiles)

--- equipmentbot.py
+++ equipmentbot.py
@@ -1,49 +1,65 @@
-from multiprocessing import Value
+#from multiprocessing import Value
 import telebot
 import get_equipment
-from config import TOKEN, TOKENSCADA
+from config import TOKEN
 
 # еще про состояние можно прочитать на https://github.com/eternnoir/pyTelegramBotAPI/blob/master/examples/custom_states.py
 
-chat_states = {}
+chat_states: dict = {}
+""" Используй это, чтобы сохранить данные состояния 
+    dict[chat_id]:(equipment_id, listfiles)
+"""
+blocked_chats = {-1001539497277}
 
-bot = telebot.TeleBot(TOKENSCADA)
+bot = telebot.TeleBot(TOKEN)
 
-def is_int(str):
+def is_int(str) -> bool:
     try:
         int(str)
         return True
     except ValueError:
         return False
 
+def is_blocked_chat(chat_id: int) -> bool:
+    """ если чат заблокирован, то выходим, не обрабатываем сообщения"""
+    return chat_id in blocked_chats
+
 @bot.message_handler(commands=["start"])
-def start(m, res=False):
-    print(f"Начинаем чат с {m.chat.id=}")
+def start(m, res=False) -> None:
+    if is_blocked_chat(m.chat.id) : return
+    first_name = m.from_user.first_name 
+    last_name = m.from_user.last_name
+    print(f"Начинаем чат с {m.chat.id=} {first_name=} {last_name=}")
     # m.from_user.first_name id last_name username
     bot.send_message(m.chat.id, f'👋 Здравствуй, {m.from_user.username}.')    
     handle_text_default(m)
 
 @bot.message_handler(commands=["id"])
 def handler_id(m, res=False):
+    if is_blocked_chat(m.chat.id) : return
     bot.send_message(m.chat.id, f'Для просмотра карточки оборудования введите номер обрудования')    
     bot.register_next_step_handler(m, equipment_inputid_handler)    
 
 @bot.message_handler(commands=["room"])
 def handler_room(m, res=False):
+    if is_blocked_chat(m.chat.id) : return
     bot.send_message(m.chat.id, f'Для вывода списка оборудования введите номер компрессорной')    
     bot.register_next_step_handler(m, room_inputroom_handler)
 
 @bot.message_handler(commands=["docs"])
 def handler_docs(m, res=False):
+    if is_blocked_chat(m.chat.id) : return
     bot.send_message(m.chat.id, f'Для вывода документации введите номер оборудования')    
     bot.register_next_step_handler(m, docs_inputid_handler)
 
 @bot.message_handler(commands=["file"])
 def handler_file(m, res=False):
+    if is_blocked_chat(m.chat.id) : return
     bot.send_message(m.chat.id, 'введена команда /file - она будет добавлена позже')
 
 @bot.message_handler(commands=["t", "test"])
 def handler_test(m, res=False):
+    if is_blocked_chat(m.chat.id) : return
     markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=False, resize_keyboard=True)
     btn0 = telebot.types.KeyboardButton('/id')
     btn1 = telebot.types.KeyboardButton('/room')
@@ -51,18 +67,21 @@ def handler_test(m, res=False):
     btn3 = telebot.types.KeyboardButton('/file')
     btncancel = telebot.types.KeyboardButton('/cancel')
     markup.add(btn0, btn1, btn2, btn3, btncancel)
-    msg = bot.send_message(m.chat.id, 'Клавиатура добавлена. Что хотите сделать?',
+    msg = bot.send_message(m.chat.id, 'Клавиатура добавлена в этот чат. Что хотите сделать?',
                            reply_markup=markup)
     # bot.register_next_step_handler(msg.msg, handler_switch)
 
-def is_canceled(m):
+
+def is_canceled(m) -> bool:
+    """ в тексте пришло '/cancel' нужно отменять данный обработчик """
     text = m.text
     return "/cancel" in text
 
 @bot.message_handler(commands=["cancel"])
 def handler_cancel(m, res=False):
-    markup = telebot.types.ReplyKeyboardRemove(selective=False)    
-    bot.send_message(m.chat.id, 'текущая операция отменена', reply_markup=markup)
+    if is_blocked_chat(m.chat.id) : return
+    # markup = telebot.types.ReplyKeyboardRemove(selective=False)    
+    bot.send_message(m.chat.id, 'текущая операция отменена')
     handle_text_default(m)
     
 
@@ -88,10 +107,11 @@ def handler_cancel(m, res=False):
 #         msg = bot.send_message(call.message.chat.id, "Ошибка бота 1")
 
 def equipment_inputid_handler(m): 
+    if is_blocked_chat(m.chat.id) : return
     if is_canceled(m):
         return handler_cancel(m)
     if not is_int(m.text):
-        msg = bot.send_message(m.chat.id, f"Введенный текст не является номером оборудования. Введите номер оборудования, например 144")
+        msg = bot.send_message(m.chat.id, f"Выводим карточку оборудования... Введенный текст не является номером оборудования. Введите номер оборудования, например 767. Для отмены введите /cancel")
         bot.register_next_step_handler(msg, equipment_inputid_handler)
         return
 
@@ -101,15 +121,16 @@ def equipment_inputid_handler(m):
     equipment = eq["equipment"]
     parametr = eq["parametr"]
     note = eq["note"]
-    strmessage = f"НАЗВАНИЕ ОБОРУДОВАНИЯ: {equipment} \nПАРАМЕТРЫ: {parametr} \nОПИСАНИЕ: {note}"
+    strmessage = f"Выводим карточку оборудования:\nНАЗВАНИЕ ОБОРУДОВАНИЯ: {equipment} \nПАРАМЕТРЫ: {parametr} \nОПИСАНИЕ: {note}"
     msg = bot.send_message(m.chat.id, strmessage)
     bot.register_next_step_handler(msg, equipment_inputid_handler)
 
 def room_inputroom_handler(m):
+    if is_blocked_chat(m.chat.id) : return
     if is_canceled(m):
         return handler_cancel(m)
     if not is_int(m.text) :
-        msg = bot.send_message(m.chat.id, "Все-таки введите номер компрессорной")
+        msg = bot.send_message(m.chat.id, "Вывод списка оборудования по номеру компрессорной. Введенный текст не является номер компрессорной. Для отмены введите /cancel")
         bot.register_next_step_handler(msg, room_inputroom_handler)
         return
    
@@ -124,46 +145,58 @@ def room_inputroom_handler(m):
     bot.register_next_step_handler(msg, room_inputroom_handler)
 
 def docs_inputid_handler(m): 
+    if is_blocked_chat(m.chat.id) : return
     if is_canceled(m):
         return handler_cancel(m)
     if not is_int(m.text) :
-        msg = bot.send_message(m.chat.id, "Введите номер оборудования - документация")
+        msg = bot.send_message(m.chat.id, "Вывод документации по номеру оборудования. Введенный текст не является номером оборудования. Для отмены введите /cancel")
         bot.register_next_step_handler(msg, docs_inputid_handler)
         return
 
     id = int(m.text)
     listfiles = get_equipment.get_listfiles(id)
     print(f'{listfiles=}')
-    if len(listfiles) == 0 :
+    if listfiles == None or len(listfiles) == 0 :
         msg = bot.send_message(m.chat.id, f"Документация по оборудованию №{id} отсутствует")    
         bot.register_next_step_handler(msg, docs_inputid_handler)
         return
 
-    chat_states[m.chat.id] = id
+    chat_states[m.chat.id] = (id, listfiles)
+
+    # вывести строки формата "number1: filename1[\n]number2: filename2..."
+    numberkeys = "\n".join(f"{i}: {v}" for i, v in enumerate(listfiles.keys()))
 
     keys = "\n".join(list(listfiles.keys()))
-    strmessage = f"{keys}"
+    strmessage = f"{numberkeys}"
     msg = bot.send_message(m.chat.id, strmessage)
     msg = bot.send_message(m.chat.id, "Введите номер файла")
     bot.register_next_step_handler(msg, file_inputnumber_handler)
 
 def file_inputnumber_handler(m): 
+    if is_blocked_chat(m.chat.id) : return
     if is_canceled(m):
         return handler_cancel(m)
+
+    (id, listfiles) = chat_states.get(m.chat.id)
+
     if not is_int(m.text) :
-        msg = bot.send_message(m.chat.id, f"Введите номер файла для оборудования №enterid")
+        msg = bot.send_message(m.chat.id, f"Запрос файла документации для оборудования №{id}. Введенный текст не является номер файла для оборудования. Для отмены введите /cancel")
         bot.register_next_step_handler(msg, file_inputnumber_handler)
         return
 
-    id = chat_states.get(m.chat.id)
-
     number = int(m.text)
+    if number < 0 or number >= len(listfiles):
+        msg = bot.send_message(m.chat.id, f"Запрос файла документации для оборудования №{id}. Введенный номер вне диапазона списка файлов. Введите число от 0 до {len(listfiles) - 1}.Для отмены введите /cancel")
+        bot.register_next_step_handler(msg, file_inputnumber_handler)
+        return
+
     strmessage = f"запрашиваем файл {number} для оборудования №{id}"
     msg = bot.send_message(m.chat.id, strmessage)
 
-    (filename, file) = get_equipment.get_file(id, number)
+    file_filename_tuple = get_equipment.get_file(id, number)
+    (filename, file) = file_filename_tuple
     print(f'{id=} {number=}')
-    if file == None:    
+    if file == None or filename == None:    
         msg = bot.send_message(m.chat.id, f"Файл {number} для оборудования №{id} отсутствует")    
         bot.register_next_step_handler(msg, file_inputnumber_handler)
         return
@@ -177,6 +210,7 @@ def file_inputnumber_handler(m):
 
 @bot.message_handler(content_types=["text"])
 def handle_text_default(m):
+    if is_blocked_chat(m.chat.id) : return
     print(f'message.chat.id={m.chat.id} {m.from_user.username} message_handler. You write {m.text}')
     bot.send_message(m.chat.id, 'Для работы с холодильным оборудованием ВЛМК выбери одну из следующих команд: \n/id - просмотр карточки по номеру оборудования \n/room - список оборудования по номеру компрессорной \n/docs - вывод документации по номеру оборудования \n/file - просмотр файла документации по номеру оборудования \n/help - для вывода данного сообщения \n/cancel - отмена выбранной команды \n/test - тоже для чего-то')
 
